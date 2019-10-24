325c0a704ca52e5abd425920abcd6425dbeb25fd
Shardul Mahadik <smahadik@linkedin.com>
2019-10-24 15:02:06 -0700

    Fix checkstyle issues

--- hive/src/main/java/org/apache/iceberg/hive/CustomHiveTableOperations.java
+++ hive/src/main/java/org/apache/iceberg/hive/CustomHiveTableOperations.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iceberg.hive;
 
 import com.google.common.collect.Lists;
@@ -142,9 +161,9 @@ public class CustomHiveTableOperations extends HiveTableOperations {
       threw = false;
     } catch (TException | UnknownHostException e) {
       if (e.getMessage().contains("Table/View 'HIVE_LOCKS' does not exist")) {
-        throw new RuntimeException("Failed to acquire locks from metastore because 'HIVE_LOCKS' doesn't "
-            + "exist, this probably happened when using embedded metastore or doesn't create a "
-            + "transactional meta table. To fix this, use an alternative metastore", e);
+        throw new RuntimeException("Failed to acquire locks from metastore because 'HIVE_LOCKS' doesn't " +
+            "exist, this probably happened when using embedded metastore or doesn't create a " +
+            "transactional meta table. To fix this, use an alternative metastore", e);
       }
 
       throw new RuntimeException(String.format("Metastore operation failed for %s.%s", database, tableName), e);
