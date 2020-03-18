1a9acf8e1b694cfc83c23ff39aec5f673a2830fe
Shardul Mahadik <shardul.m@somaiya.edu>
2020-03-18 13:56:24 -0700

    Support reading tables with only Hive metadata (#23)
    * Support reading tables with Hive metadata if Iceberg metadata is not available

--- core/src/main/java/org/apache/iceberg/BaseFileScanTask.java
+++ core/src/main/java/org/apache/iceberg/BaseFileScanTask.java
@@ -29,7 +29,7 @@ import java.util.NoSuchElementException;
 import org.apache.iceberg.expressions.Expression;
 import org.apache.iceberg.expressions.ResidualEvaluator;
 
-class BaseFileScanTask implements FileScanTask {
+public class BaseFileScanTask implements FileScanTask {
   private final DataFile file;
   private final String schemaString;
   private final String specString;
@@ -37,7 +37,7 @@ class BaseFileScanTask implements FileScanTask {
 
   private transient PartitionSpec spec = null;
 
-  BaseFileScanTask(DataFile file, String schemaString, String specString, ResidualEvaluator residuals) {
+  public BaseFileScanTask(DataFile file, String schemaString, String specString, ResidualEvaluator residuals) {
     this.file = file;
     this.schemaString = schemaString;
     this.specString = specString;
