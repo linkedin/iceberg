c60a14077c7b4b81b0848f10657004bca754ad70
Sushant Raikar <sraikar@linkedin.com>
2020-09-17 12:07:34 -0700

    Hive: Made Predicate Pushdown dynamic based on the Hive Version

--- mr/src/main/java/org/apache/iceberg/mr/hive/HiveIcebergInputFormat.java
+++ mr/src/main/java/org/apache/iceberg/mr/hive/HiveIcebergInputFormat.java
@@ -23,9 +23,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hive.ql.exec.SerializationUtilities;
 import org.apache.hadoop.hive.ql.io.CombineHiveInputFormat;
-import org.apache.hadoop.hive.ql.io.sarg.ConvertAstToSearchArg;
 import org.apache.hadoop.hive.ql.io.sarg.SearchArgument;
 import org.apache.hadoop.hive.ql.plan.ExprNodeGenericFuncDesc;
 import org.apache.hadoop.hive.ql.plan.TableScanDesc;
@@ -34,6 +32,7 @@ import org.apache.hadoop.mapred.InputSplit;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.RecordReader;
 import org.apache.hadoop.mapred.Reporter;
+import org.apache.iceberg.common.DynMethods;
 import org.apache.iceberg.data.Record;
 import org.apache.iceberg.expressions.Expression;
 import org.apache.iceberg.mr.InputFormatConfig;
@@ -48,15 +47,27 @@ public class HiveIcebergInputFormat extends MapredIcebergInputFormat<Record>
                                     implements CombineHiveInputFormat.AvoidSplitCombination {
 
   private static final Logger LOG = LoggerFactory.getLogger(HiveIcebergInputFormat.class);
+  private static final DynMethods.StaticMethod DESERIALIZE_OBJECT = DynMethods.builder("deserializeObject")
+      .impl("org.apache.hadoop.hive.ql.exec.SerializationUtilities", String.class, Class.class)
+      .impl("org.apache.hadoop.hive.ql.exec.Utilities", String.class, Class.class)
+      .buildStatic();
+  private static final DynMethods.StaticMethod CONSTRUCT_SARG_HIVE_1 = DynMethods.builder("create")
+      .impl("org.apache.hadoop.hive.ql.io.sarg.SearchArgumentFactory", ExprNodeGenericFuncDesc.class)
+      .orNoop()
+      .buildStatic();
+  private static final DynMethods.StaticMethod CONSTRUCT_SARG_HIVE_2 = DynMethods.builder("create")
+      .impl("org.apache.hadoop.hive.ql.io.sarg.ConvertAstToSearchArg",
+          Configuration.class, ExprNodeGenericFuncDesc.class)
+      .orNoop()
+      .buildStatic();
 
   @Override
   public InputSplit[] getSplits(JobConf job, int numSplits) throws IOException {
     // Convert Hive filter to Iceberg filter
     String hiveFilter = job.get(TableScanDesc.FILTER_EXPR_CONF_STR);
     if (hiveFilter != null) {
-      ExprNodeGenericFuncDesc exprNodeDesc = SerializationUtilities
-              .deserializeObject(hiveFilter, ExprNodeGenericFuncDesc.class);
-      SearchArgument sarg = ConvertAstToSearchArg.create(job, exprNodeDesc);
+      ExprNodeGenericFuncDesc exprNodeDesc = DESERIALIZE_OBJECT.invoke(hiveFilter, ExprNodeGenericFuncDesc.class);
+      SearchArgument sarg = constructSearchArgument(job, exprNodeDesc);
       try {
         Expression filter = HiveIcebergFilterFactory.generateFilterExpression(sarg);
         job.set(InputFormatConfig.FILTER_EXPRESSION, SerializationUtil.serializeToBase64(filter));
@@ -85,5 +96,13 @@ public class HiveIcebergInputFormat extends MapredIcebergInputFormat<Record>
   @Override
   public boolean shouldSkipCombine(Path path, Configuration conf) {
     return true;
+  }
+
+  private SearchArgument constructSearchArgument(JobConf job, ExprNodeGenericFuncDesc exprNodeDesc) {
+    SearchArgument searchArgument = CONSTRUCT_SARG_HIVE_2.invoke(job, exprNodeDesc);
+    if (searchArgument == null) {
+      searchArgument = CONSTRUCT_SARG_HIVE_1.invoke(exprNodeDesc);
+    }
+    return searchArgument;
   }
 }
