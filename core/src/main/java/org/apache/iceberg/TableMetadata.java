c3ad35d72e4d9df716f563e5e5280971c92bbcb9
Shardul Mahadik <smahadik@linkedin.com>
2021-01-11 11:17:46 -0800

    Hive Metadata Scan: Support case insensitive name mapping (#52)

--- core/src/main/java/org/apache/iceberg/TableMetadata.java
+++ core/src/main/java/org/apache/iceberg/TableMetadata.java
@@ -101,6 +101,17 @@ public class TableMetadata implements Serializable {
         ImmutableList.of(), ImmutableList.of());
   }
 
+  public static TableMetadata newTableMetadataWithoutFreshIds(Schema schema,
+      PartitionSpec spec,
+      String location,
+      Map<String, String> properties) {
+    return new TableMetadata(null, DEFAULT_TABLE_FORMAT_VERSION, UUID.randomUUID().toString(), location,
+        INITIAL_SEQUENCE_NUMBER, System.currentTimeMillis(),
+        -1, schema, INITIAL_SPEC_ID, ImmutableList.of(spec),
+        ImmutableMap.copyOf(properties), -1, ImmutableList.of(),
+        ImmutableList.of(), ImmutableList.of());
+  }
+
   public static class SnapshotLogEntry implements HistoryEntry {
     private final long timestampMillis;
     private final long snapshotId;
