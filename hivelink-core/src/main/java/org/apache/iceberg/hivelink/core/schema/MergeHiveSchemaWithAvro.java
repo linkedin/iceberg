26b7e112f96aee934df66d08a829e2d4f762214c
Limian (Raymond) Zhang <razhang@linkedin.com>
2022-08-16 11:28:34 -0700

    Fix MergeHiveSchemaWithAvro to make it copy full Avro schema attributes (#120)
    * Fix MergeHiveSchemaWithAvro to make it copy full Avro schema attributes

--- hivelink-core/src/main/java/org/apache/iceberg/hivelink/core/schema/MergeHiveSchemaWithAvro.java
+++ hivelink-core/src/main/java/org/apache/iceberg/hivelink/core/schema/MergeHiveSchemaWithAvro.java
@@ -21,6 +21,7 @@ package org.apache.iceberg.hivelink.core.schema;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.avro.JsonProperties;
 import org.apache.avro.LogicalTypes;
@@ -113,6 +114,7 @@ public class MergeHiveSchemaWithAvro extends HiveSchemaWithPartnerVisitor<Schema
     // if there was no matching Avro list, or if matching Avro list was an option, return an optional list
     boolean shouldResultBeOptional = partner == null || AvroSchemaUtil.isOptionSchema(partner);
     Schema result = Schema.createArray(elementResult);
+    copySchemaProps(partner, result);
     return shouldResultBeOptional ? AvroSchemaUtil.toOption(result) : result;
   }
 
@@ -123,6 +125,7 @@ public class MergeHiveSchemaWithAvro extends HiveSchemaWithPartnerVisitor<Schema
     // if there was no matching Avro map, or if matching Avro map was an option, return an optional map
     boolean shouldResultBeOptional = partner == null || AvroSchemaUtil.isOptionSchema(partner);
     Schema result = Schema.createMap(valueResult);
+    copySchemaProps(partner, result);
     return shouldResultBeOptional ? AvroSchemaUtil.toOption(result) : result;
   }
 
@@ -150,6 +153,14 @@ public class MergeHiveSchemaWithAvro extends HiveSchemaWithPartnerVisitor<Schema
     // TODO: Check if schema is compatible with partner
     //       Also do type promotion if required, schema = string & partner = enum, schema = bytes & partner = fixed, etc
     return schema;
+  }
+
+  private static void copySchemaProps(Schema from, Schema to) {
+    if (from != null) {
+      for (Map.Entry<String, Object> prop : from.getObjectProps().entrySet()) {
+        to.addProp(prop.getKey(), prop.getValue());
+      }
+    }
   }
 
   /**
