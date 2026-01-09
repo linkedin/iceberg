10475c958edc95f089545fb6b465e4d4d68df32f
CI Builder <ci@example.com>
2026-01-08 16:12:15 -0800

    Add delete file replication as table and spark-sql property.

--- spark/v3.3/spark/src/main/java/org/apache/iceberg/spark/SparkSQLProperties.java
+++ spark/v3.3/spark/src/main/java/org/apache/iceberg/spark/SparkSQLProperties.java
@@ -69,4 +69,7 @@ public class SparkSQLProperties {
 
   // Controls the spark input split size.
   public static final String SPLIT_SIZE = "spark.sql.iceberg.split-size";
+
+  // Controls the replication factor for delete files
+  public static final String DELETE_FILE_REPLICATION = "spark.sql.iceberg.delete-file-replication";
 }
